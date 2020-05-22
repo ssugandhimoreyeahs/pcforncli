a697a4101981f210b399b875cf056d18929b130c
ssugandhimoreyeahs <ssugandhi@moreyeahs.in>
2020-05-22 21:04:28 +0530

    final

--- src/app/component/Root/index.js
+++ src/app/component/Root/index.js
@@ -1,5 +1,11 @@
-import React,{ Fragment } from "react";
-import { View,StyleSheet, SafeAreaView, StatusBar, Platform } from "react-native";
+import React, { Fragment } from "react";
+import {
+  View,
+  StyleSheet,
+  SafeAreaView,
+  StatusBar,
+  Platform,
+} from "react-native";
 
 const MyWrapper = Platform.select({
   ios: SafeAreaView,
@@ -8,26 +14,21 @@ const MyWrapper = Platform.select({
 const Root = ({ children }) => {
   return (
     <Fragment>
-      <MyWrapper
-        
-        style={style.customStatusBar}
-      />
-      <SafeAreaView style={style.body} >
-      { children }
-      </SafeAreaView>
+      <MyWrapper style={style.customStatusBar} />
+      <SafeAreaView style={style.body}>{children}</SafeAreaView>
     </Fragment>
   );
 };
 
 const style = StyleSheet.create({
-    customStatusBar : {
-        flex: 0, 
-        backgroundColor: "#9c9a9a"
-    },
-    body: { 
-        flex: 1
-    }
-})
+  customStatusBar: {
+    flex: 0,
+    backgroundColor: "#9c9a9a",
+  },
+  body: {
+    flex: 1,
+  },
+});
 export default Root;
 
 /*
